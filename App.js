e29950d7c8d06998b39f58891e63e75c14ad65e1
Ahamed Althaf <ahamedalthaf@Ahameds-MacBook-Pro.local>
2025-08-10 12:42:31 +0530

    Rendered App Using PARCEL

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
-const heading1 = React.createElement("h1",{}, "Hello world from React CDN");
+import React from "react";
+import ReactDOM from "react-dom/client"
+const heading1 = React.createElement("h1",{id: "heading1"}, "Hello world from React");
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(heading1);
